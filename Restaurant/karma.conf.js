8373875b52a90330f628d91d2f3b992d8cc04785
Priya Kumari <priyakumariraj53@gmail.com>
2022-07-25 08:22:54 +0530

    Commit Message

--- Restaurant/karma.conf.js
+++ Restaurant/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/con-fusion'),
+      dir: require('path').join(__dirname, './coverage/restaurant'),
       subdir: '.',
       reporters: [
         { type: 'html' },
